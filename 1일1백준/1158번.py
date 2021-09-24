8c7bf7aad91fc577222706e965bafdba435989c2
Dowon Yun <90703740+Dowon-Study@users.noreply.github.com>
2021-09-25 04:43:17 +0900

    Update 1158번.py

--- 1일1백준/1158번.py
+++ 1일1백준/1158번.py
@@ -1,1 +1,11 @@
-1
+count, num = map(int, input().split())
+stack = [i for i in range(1, count+1)]
+result = []
+num_1 = num-1
+while len(stack):
+    if num_1 >= len(stack):
+        num_1 = num_1 % len(stack)
+    else:
+        result.append(str(stack.pop(num_1)))
+        num_1 += (num-1)
+print("<", ", ".join(result), ">", sep='')
